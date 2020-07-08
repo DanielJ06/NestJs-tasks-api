e8196ba9a65b1c0c97347042795d0a60b82b469f
Danielj06 <daniel.j.rodrigues06@gmail.com>
2020-07-08 11:00:05 -0300

    Feature: delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -20,5 +20,10 @@ export class TasksController {
   @Post()
     createNewTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
       return this.tasksServices.createNewTask(createTaskDto);
+    }
+
+  @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+      this.tasksServices.deleteTask(id);
     }
 }
