4660d66678cbeb6607cdaf35b86147f89469ef29
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-07-14 15:49:30 -0300

    Auth credentials DTO Regular expression for strong passwords

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
+    { message: 'Password too week' }
+  )
+  password: string;
+}
