ef957f17a491de5b85e8a65c365bdb8d2c7fcd7e
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-07-16 16:59:47 -0300

    Sign token on signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
-  async signUp(authCredentialsDto: AuthCredentialsDto) {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.singUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
-    const result = await this.userRepository.validateUserPassword(authCredentialsDto);
-    console.log(result);
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ token: string}> {
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    
+    const payload: JwtPayload = { username };
+    const token = await this.jwtService.sign(payload);
+
+    return { token };
   }
 }
