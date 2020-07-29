0fbc99289873e5b0e6442cd89537a6bf7b7b81f6
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-07-28 22:18:51 -0300

    Updating a User's task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -40,10 +40,14 @@ export class TasksController {
       return this.tasksServices.createNewTask(createTaskDto, user);
     }
 
-  // @Patch('/:id/status')
-  //   updateTaskStatus(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Promise<Task> {
-  //     return this.tasksServices.updateTaskStatus(id, status);
-  //   }
+  @Patch('/:id/status')
+    updateTaskStatus(
+      @Param('id', ParseIntPipe) id: number, 
+      @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+      @GetUser() user: User
+    ): Promise<Task> {
+      return this.tasksServices.updateTaskStatus(id, status, user);
+    }
 
   @Delete('/:id')
     deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
